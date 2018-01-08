aa67cb603b88160aae2246151834cc127b913958
Yizhi Liu <yizhiliu@amazon.com>
2018-01-08 10:14:09 -0800

    catch up gemm perf of numpy-openblas

--- opt_gemm.py
+++ opt_gemm.py
@@ -2,6 +2,8 @@ import tvm
 import numpy
 import time
 
+# TVM_NUM_THREADS=4 python2 opt_gemm.py
+
 # The size of the square matrix
 N = 1024
 # The default tensor type in tvm
@@ -14,11 +16,11 @@ b = tvm.nd.array(numpy.random.rand(N, N).astype(dtype), tvm.cpu(0))
 answer = numpy.dot(a.asnumpy(), b.asnumpy())
 
 # Algorithm
-bn = 8
 k = tvm.reduce_axis((0, N), 'k')
 A = tvm.placeholder((N, N), name = 'A')
 B = tvm.placeholder((N, N), name = 'B')
-# We have to re-write the algorithm slightly.
+
+bn = 8
 packedB = tvm.compute((N / bn, N, bn), lambda x, y, z: B[y, x * bn + z], name = 'packedB')
 C = tvm.compute(A.shape,
                 lambda x, y: tvm.sum(A[x, k] * packedB[y / bn, k, y % bn], axis = k),
@@ -26,25 +28,53 @@ C = tvm.compute(A.shape,
 
 # Same schedule
 s = tvm.create_schedule(C.op)
-yo, xo, yi, xi = s[C].tile(C.op.axis[1], C.op.axis[0], bn, bn)
-s[C].reorder(yo, xo, k, yi, xi)
-s[C].vectorize(yi)
+x, y, z = s[packedB].op.axis
+# s[packedB].reorder(y, x, z)
+s[packedB].vectorize(z)
 
+# CC = s.cache_write(C, 'local')
+AA = s.cache_read(A, 'local', [C])
+BB = s.cache_read(packedB, 'local', [C])
+
+yo, xo, yi, xi = s[C].tile(C.op.axis[1], C.op.axis[0], bn, bn)
+ko, ki = s[C].split(k, factor=4)
+
+s[C].reorder(yo, xo, ko, ki, yi, xi)
+s[C].vectorize(yi)
+s[C].unroll(xi)
+s[C].unroll(ki)
+
+# s[CC].compute_at(s[C], xo)
+# yi, xi = s[CC].op.axis
+# k, = s[CC].op.reduce_axis
+# ko, ki = s[CC].split(k, factor=4)
+#
+# s[CC].reorder(ko, ki, yi, xi)
+# s[CC].vectorize(yi)
+# s[CC].unroll(ki)
+# s[CC].unroll(xi)
+
+s[AA].compute_at(s[C], ko)
+ao, ai = s[AA].op.axis
+s[AA].vectorize(ai)
+s[AA].unroll(ao)
+
+s[BB].compute_at(s[C], ko)
+print(s[BB].op.axis)
+_, bo, bi = s[BB].op.axis
+s[BB].vectorize(bi)
+s[BB].unroll(bo)
+
+s[C].parallel(yo)
+
+print(tvm.lower(s, [A, B, C], simple_mode=True))
 func = tvm.build(s, [A, B, C], target=target, name = 'mmult')
 assert func
 # We can accelerate it almost 3x compared with the previous schedule.
-evaluator = func.time_evaluator(func.entry_name, tvm.cpu(0), number = 5)
+evaluator = func.time_evaluator(func.entry_name, tvm.cpu(0), number = 100)
 c = tvm.nd.array(numpy.zeros((N, N), dtype = dtype), tvm.cpu(0))
 print('Opt3: %f' % evaluator(a, b, c).mean)
 
-##################################################################################################
-# Summary
-# -------
-# After applying three main tricks, we can achieve almost 90% performance of numpy.
-# Further observation is required to catch up with the performance of numpy.
-#
-
-# TODO(Jian Weng): Catch up with the performance of numpy.
 _a = a.asnumpy()
 _b = b.asnumpy()
 now = time.clock()
