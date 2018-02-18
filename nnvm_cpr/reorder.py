d75e3786fa1bc74d5bcf5ca0bc70ceb24aa81206
Yizhi Liu <yizhiliu@amazon.com>
2018-02-17 22:38:52 -0800

    nnvm op reorder

--- nnvm_cpr/reorder.py
+++ nnvm_cpr/reorder.py
@@ -6,21 +6,27 @@ from tvm.contrib import graph_runtime
 
 import numpy as np
 
+# (oc, ic, h, w)
+oc, ic, h, w = 3, 4, 2, 2
+shape = (oc, ic, h, w)
+oc_bn, ic_bn = 3, 2
+# (OC, IC, h, w, ic, oc)
+out_shape = (oc//oc_bn, ic//ic_bn, h, w, ic_bn, oc_bn)
+
 x = sym.Variable("x")
 # y = sym.Variable("y")
 # z = sym.elemwise_add(x, sym.sqrt(y))
-z = sym.transpose(x)
+# z = sym.reshape(x, shape=out_shape)
+z = sym.reorder(x, oc_bn=3, ic_bn=2)
 compute_graph = nnvm.graph.create(z)
 print("-------compute graph-------")
 print(compute_graph.ir())
 
-shape = (3, 2)
-out_shape = (2, 3)
 deploy_graph, lib, params = nnvm.compiler.build(
     compute_graph, target="llvm", shape={"x": shape}, dtype="float32")
 
 module = graph_runtime.create(deploy_graph, lib, tvm.cpu(0))
-x_np = np.array([[1, 2], [3, 4], [5, 6]]).astype("float32")
+x_np = np.random.uniform(0, 255, size=shape).astype("float32")
 print(x_np)
 # y_np = np.array([[4, 4], [4, 4], [4, 4]]).astype("float32")
 # set input to the graph module
