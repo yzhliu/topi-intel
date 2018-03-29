15805eae4dd7b72c9c3c63427768cf4088daaa51
Yizhi Liu <javelinjs@gmail.com>
2018-03-29 21:10:56 +0700

    add conv2d nchw kernel pack only

--- e2e_general_pack/schedule_kernel_pack_only/avx512_conv_1x1.py
+++ e2e_general_pack/schedule_kernel_pack_only/avx512_conv_1x1.py
@@ -5,7 +5,8 @@ import topi
 from topi.nn.conv2d import conv2d, _get_schedule
 from topi.nn.conv2d import _get_workload
 
-from topi.util import get_const_tuple, get_const_int
+from topi.util import get_const_tuple
+from topi.nn.util import infer_pad
 from topi.nn.pad import pad
 
 from collections import namedtuple
@@ -13,30 +14,20 @@ from collections import namedtuple
 AVX512Conv1x1Fwd = namedtuple('AVX512Conv1x1Fwd', ['ic_bn', 'oc_bn', 'oh_factor', 'ow_factor'])
 
 def infer_stride(data, kernel, out):
-    _, _, IH, IW, _ = data.shape
+    from topi.util import get_const_int
+    _, _, IH, IW = data.shape
     CO, _, _, co, KH, KW = kernel.shape
     CO *= co
-    _, _, OH, OW, _ = out.shape
+    _, _, OH, OW = out.shape
     hstride = (IH - KH) // (OH - 1)
     wstride = (IW - KW) // (OW - 1)
     return get_const_int(hstride), get_const_int(wstride)
-
-def infer_pad(data, data_pad):
-    if data_pad is None:
-        return 0, 0
-    _, _, IH, IW, _ = data.shape
-    _, _, TH, TW, _ = data_pad.shape
-    hpad = (TH - IH) // 2
-    wpad = (TW - IW) // 2
-    return get_const_int(hpad), get_const_int(wpad)
 
 def get_workload(data, kernel, stride, padding, out_dtype):
     """ Get the workload structure. """
     CO, CI, ci, co, KH, KW = [x.value for x in kernel.shape]
     ori_kernel = tvm.placeholder((CO*co, CI*ci, KH, KW))
-    n, _, h, w, _ = [x.value for x in data.shape]
-    original_data = tvm.placeholder((n, CI * ci, h, w))
-    return _get_workload(original_data, ori_kernel, stride, padding, out_dtype)
+    return _get_workload(data, ori_kernel, stride, padding, out_dtype)
 
 def _declaration_conv(data, kernel, stride, padding, out_dtype):
     assert data.shape[0].value == 1, "only support batch size=1 convolution on rasp"
@@ -46,7 +37,7 @@ def _declaration_conv(data, kernel, stride, padding, out_dtype):
     HPAD, WPAD = wkl.hpad, wkl.wpad
     HSTR, WSTR = wkl.hstride, wkl.wstride
 
-    batch_size, in_channel_chunk, in_height, in_width, in_channel_block = get_const_tuple(data.shape)
+    batch_size, in_channel, in_height, in_width = get_const_tuple(data.shape)
     num_filter, _, _, co, kernel_height, kernel_width = get_const_tuple(kernel.shape)
     num_filter *= co
 
@@ -59,19 +50,11 @@ def _declaration_conv(data, kernel, stride, padding, out_dtype):
     # input: c, h, w
     DOPAD = (HPAD != 0 and WPAD != 0)
     if DOPAD:
-        data_pad = pad(data, (0, 0, HPAD, WPAD, 0), name="data_pad")
+        data_pad = pad(data, (0, 0, HPAD, WPAD), name="data_pad")
     else:
         data_pad = data
-
-    in_channel = in_channel_block * in_channel_chunk
-    if in_channel_block != sch.ic_bn:
-        print('WARNING!!! (1x1) in_channel_block=%d vs sch.ic_bn=%d' % (in_channel_block, sch.ic_bn))
-        shape = (batch_size, in_channel // sch.ic_bn, pad_height, pad_width, sch.ic_bn)
-        data_vec = tvm.compute(shape, lambda n, C, h, w, c:
-            data_pad[n, (C * sch.ic_bn + c) // in_channel_block, h, w, (C * sch.ic_bn + c) % in_channel_block],
-                               tag='conv2d_data_pack')
-    else:
-        data_vec = data_pad
+    shape = (batch_size, in_channel // sch.ic_bn, pad_height, pad_width, sch.ic_bn)
+    data_vec = tvm.compute(shape, lambda n, C, h, w, c: data_pad[n, C * sch.ic_bn + c, h, w])
 
     kernel_pack = kernel
 
@@ -80,9 +63,12 @@ def _declaration_conv(data, kernel, stride, padding, out_dtype):
     conv = tvm.compute(oshape, lambda n, oc_chunk, oh, ow, oc_block:
         tvm.sum(data_vec[n, ic // sch.ic_bn, oh * HSTR, ow * WSTR, ic % sch.ic_bn].astype(out_dtype) *
                 kernel_pack[oc_chunk, ic // sch.ic_bn, ic % sch.ic_bn, oc_block, 0, 0],
-                axis=[ic]), name='conv2d_nChwc', tag='conv2d_nChwc')
+                axis=[ic]), name='conv')
 
-    return conv
+    oshape = (batch_size, num_filter, out_height, out_width)
+    unpack = tvm.compute(oshape, lambda n, oc, oh, ow: conv[n, oc // sch.oc_bn, oh, ow, oc % sch.oc_bn],
+                         tag='conv2d_nchw')
+    return unpack
 
 
 def _schedule_conv(s, data, data_pad, data_vec, kernel, conv_out, output, last):
@@ -96,33 +82,23 @@ def _schedule_conv(s, data, data_pad, data_vec, kernel, conv_out, output, last):
     wkl = get_workload(data, kernel, stride, padding, output.dtype)
     sch = _get_schedule(wkl)
 
-    HPAD, WPAD = wkl.hpad, wkl.wpad
-    DOPAD = (HPAD != 0 and WPAD != 0)
-
     # A, W = data, kernel_pack
     A0, A1 = data_pad, data_vec
     # schedule data
-    if isinstance(s[A1].op, tvm.tensor.ComputeOp): # and  "conv2d_data_pack" in s[A1].op.tag:
-        if DOPAD and "conv2d_data_pack" in s[A1].op.tag:
-            s[A0].compute_inline()
-        batch, ic_chunk, ih, iw, ic_block = s[A1].op.axis
-        parallel_axis = s[A1].fuse(ic_chunk, ih)
-        s[A1].parallel(parallel_axis)
+    if A0 is not None:
+        s[A0].compute_inline()
+    batch, ic_chunk, ih, ic_block, iw = s[A1].op.axis
+    parallel_axis = s[A1].fuse(ic_chunk, ih)
+    s[A1].parallel(parallel_axis)
 
     C, O0, O = conv_out, output, last
     CC = s.cache_write(C, 'global')
 
     batch, oc_chunk, oh, ow, oc_block = s[C].op.axis
     oh_outer, oh_inner = s[C].split(oh, factor=sch.oh_factor)
-    ow_outer, ow_inner = s[C].split(ow, factor=sch.ow_factor)
-    s[C].reorder(oc_chunk, oh_outer, ow_outer, oh_inner, ow_inner, oc_block)
     s[C].vectorize(oc_block)
 
-    parallel_axis = s[C].fuse(oc_chunk, oh_outer)
-    s[CC].compute_at(s[C], parallel_axis)
-    if C == O:
-        s[C].parallel(parallel_axis)
-
+    s[CC].compute_at(s[C], oh_outer)
     _, oc_chunk, oh, ow, oc_block = s[CC].op.axis
     ic, = s[CC].op.reduce_axis
 
@@ -132,7 +108,6 @@ def _schedule_conv(s, data, data_pad, data_vec, kernel, conv_out, output, last):
     ow_outer, ow_inner = s[CC].split(ow, factor=sch.ow_factor)
 
     s[CC].reorder(oc_chunk, oh_outer, ow_outer, ic_chunk, ic_block, oh_inner, ow_inner, oc_block)
-    s[CC].fuse(oc_chunk, oh_outer)
     s[CC].vectorize(oc_block)
 
     s[CC].unroll(ow_inner)
@@ -140,19 +115,17 @@ def _schedule_conv(s, data, data_pad, data_vec, kernel, conv_out, output, last):
 
     if O0 != O:
         s[O0].compute_inline()
+    batch, oc, oh, ow = s[O].op.axis
 
-    if C != O:
-        batch, oc_chunk, oh, ow, oc_block = s[O].op.axis
+    oc_chunk, oc_block = s[O].split(oc, factor=sch.oc_bn)
+    oh_outer, oh_inner = s[O].split(oh, factor=sch.oh_factor)
+    ow_outer, ow_inner = s[O].split(ow, factor=sch.ow_factor)
+    s[O].reorder(oc_chunk, oh_outer, ow_outer, oh_inner, ow_inner, oc_block)
 
-        # oc_chunk, oc_block = s[O].split(oc, factor=sch.oc_bn)
-        oh_outer, oh_inner = s[O].split(oh, factor=sch.oh_factor)
-        ow_outer, ow_inner = s[O].split(ow, factor=sch.ow_factor)
-        s[O].reorder(oc_chunk, oh_outer, ow_outer, oh_inner, ow_inner, oc_block)
+    parallel_axis = s[O].fuse(oc_chunk, oh_outer)
+    s[C].compute_at(s[O], parallel_axis)
+    s[O].vectorize(oc_block)
 
-        parallel_axis = s[O].fuse(oc_chunk, oh_outer)
-        s[C].compute_at(s[O], parallel_axis)
-        s[O].vectorize(oc_block)
-
-        s[O].parallel(parallel_axis)
+    s[O].parallel(parallel_axis)
 
     return s
--- e2e_general_pack/schedule_kernel_pack_only/avx512_conv_fwd.py
+++ e2e_general_pack/schedule_kernel_pack_only/avx512_conv_fwd.py
@@ -5,17 +5,16 @@ from . import avx512_conv_common, avx512_conv_1x1
 from .avx512_conv_common import AVX512ConvCommonFwd
 from .avx512_conv_1x1 import AVX512Conv1x1Fwd
 
-import nnvm
 import nnvm.symbol as sym
 from nnvm.top import registry as reg
 
 import tvm
 from topi.nn.conv2d import conv2d, _get_schedule
-from topi.util import get_const_tuple, get_const_int
-from topi.nn.conv2d_prepack import conv2d_nChwc
+from topi.nn.conv2d_prepack import conv2d_nchw_kernel_packed
 from topi.nn.conv2d import _WORKLOADS, Workload
 from topi.nn.conv2d import _get_workload
 from topi import generic
+from topi.util import get_const_tuple, get_const_int
 from topi.nn.util import infer_pad, infer_stride
 from topi import tag
 
@@ -31,7 +30,7 @@ _SCHEDULES = [
     AVX512ConvCommonFwd(16, fp32_vec_len, 14, False),
     AVX512Conv1x1Fwd(16, fp32_vec_len, 2, 14),
     AVX512ConvCommonFwd(16, fp32_vec_len, 14, True),
-    AVX512ConvCommonFwd(16, fp32_vec_len, 7, True),
+    AVX512ConvCommonFwd(16, 32, 7, True),
     AVX512Conv1x1Fwd(16, fp32_vec_len, 1, 7),
     AVX512ConvCommonFwd(16, fp32_vec_len, 7, True),
     # workloads of resnet34_v1 on imagenet, no extra workload required
@@ -40,15 +39,15 @@ _SCHEDULES = [
     AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=28),
     AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=28),
     AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=28),
-    AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=28),
-    AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=28),
-    AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=14),
+    AVX512Conv1x1Fwd(ic_bn=16, oc_bn=32, oh_factor=2, ow_factor=28),
+    AVX512Conv1x1Fwd(ic_bn=16, oc_bn=32, oh_factor=2, ow_factor=28),
+    AVX512Conv1x1Fwd(ic_bn=16, oc_bn=32, oh_factor=2, ow_factor=14),
     AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=14),
     AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=14),
     AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=14),
     AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=7),
     AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=7),
-    AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=7),
+    AVX512Conv1x1Fwd(ic_bn=16, oc_bn=32, oh_factor=2, ow_factor=7),
     AVX512Conv1x1Fwd(ic_bn=16, oc_bn=16, oh_factor=2, ow_factor=7),
     # workloads of resnet101_v1 on imagenet, no extra workload required
     # workloads of resnet152_v1 on imagenet, no extra workload required
@@ -56,8 +55,8 @@ _SCHEDULES = [
     # workloads of resnet34_v2 on imagenet, no extra workload required
     # workloads of resnet50_v2 on imagenet
     AVX512ConvCommonFwd(ic_bn=16, oc_bn=16, reg_n=28, unroll_kw=True),
-    AVX512ConvCommonFwd(ic_bn=16, oc_bn=16, reg_n=14, unroll_kw=False),
-    AVX512ConvCommonFwd(ic_bn=16, oc_bn=16, reg_n=7, unroll_kw=True),
+    AVX512ConvCommonFwd(ic_bn=16, oc_bn=32, reg_n=14, unroll_kw=False),
+    AVX512ConvCommonFwd(ic_bn=16, oc_bn=32, reg_n=7, unroll_kw=True),
     # workloads of resnet101_v2 on imagenet, no extra workload required
     # workloads of resnet152_v2 on imagenet, no extra workload required
     # workloads of mobilenet 1.0 on imagenet 10 29-38
@@ -134,6 +133,37 @@ _SCH_TO_SCH_FUNC = {
 
 @_get_schedule.register("cpu", override=True)
 def _get_schedule_conv(wkl):
+    # workloads = _WORKLOADS + [
+    #     # resnet 50 workloads
+    #     Workload(in_dtype='float32', out_dtype='float32', height=56, width=56, in_filter=64, out_filter=256,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=1, wstride=1),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=56, width=56, in_filter=256, out_filter=64,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=1, wstride=1),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=56, width=56, in_filter=256, out_filter=128,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=2, wstride=2),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=28, width=28, in_filter=128, out_filter=512,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=1, wstride=1),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=56, width=56, in_filter=256, out_filter=512,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=2, wstride=2),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=28, width=28, in_filter=512, out_filter=128,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=1, wstride=1),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=28, width=28, in_filter=512, out_filter=256,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=2, wstride=2),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=14, width=14, in_filter=256, out_filter=1024,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=1, wstride=1),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=28, width=28, in_filter=512, out_filter=1024,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=2, wstride=2),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=14, width=14, in_filter=1024, out_filter=256,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=1, wstride=1),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=14, width=14, in_filter=1024, out_filter=512,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=2, wstride=2),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=7, width=7, in_filter=512, out_filter=2048,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=1, wstride=1),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=14, width=14, in_filter=1024, out_filter=2048,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=2, wstride=2),
+    #     Workload(in_dtype='float32', out_dtype='float32', height=7, width=7, in_filter=2048, out_filter=512,
+    #              hkernel=1, wkernel=1, hpad=0, wpad=0, hstride=1, wstride=1)
+    # ]
     workloads = [
         # workloads of resnet18_v1 on imagenet 12 0-11
         Workload('float32', 'float32', 224, 224, 3, 64, 7, 7, 3, 3, 2, 2),
@@ -512,31 +542,24 @@ def _get_schedule_conv(wkl):
     if wkl not in workloads:
         raise ValueError("no schedule for such workload: {}".format(wkl))
     idx = workloads.index(wkl)
-    if idx >= len(_SCHEDULES):
-        sch = AVX512ConvCommonFwd(16, fp32_vec_len, 28, False)
-    else:
-        sch = _SCHEDULES[idx]
+    sch = _SCHEDULES[idx]
     return sch
 
 
 @reg.register_weight_prepack("conv2d")
 def weight_prepack_conv2d(attrs, inputs, tinfos):
     import ast
-    data_sym = inputs[0]
     data = tinfos[0]
     kernel = tinfos[1]
     padding = ast.literal_eval(attrs['padding'])
     stride = ast.literal_eval(attrs['strides'])
     wkl = _get_workload(data, kernel, stride, padding, 'float32')
     sch = _get_schedule_conv(wkl)
-    print(sch)
     is_kernel_1x1 = isinstance(sch, AVX512Conv1x1Fwd)
 
     ic_bn, oc_bn = sch.ic_bn, sch.oc_bn
 
-    new_attrs = {k : attrs[k] for k in attrs.keys()}
-    new_attrs['ic_bn'] = ic_bn
-    new_attrs['oc_bn'] = oc_bn
+    new_attrs = {k: attrs[k] for k in attrs.keys()}
     new_attrs.pop('layout', None)
 
     kernel_sym = inputs[1]
@@ -544,7 +567,7 @@ def weight_prepack_conv2d(attrs, inputs, tinfos):
     OC = oc // oc_bn
     IC = ic // ic_bn
     trans_kernel = sym.transpose(kernel_sym, axes=(1, 2, 3, 0))
-    trans_kernel = sym.reshape(trans_kernel, shape = (ic, h, w, OC, oc_bn))
+    trans_kernel = sym.reshape(trans_kernel, shape=(ic, h, w, OC, oc_bn))
     trans_kernel = sym.transpose(trans_kernel, axes=(1, 2, 3, 4, 0))
     trans_kernel = sym.reshape(trans_kernel, shape=(h, w, OC, oc_bn, IC, ic_bn))
     if is_kernel_1x1:
@@ -557,40 +580,24 @@ def weight_prepack_conv2d(attrs, inputs, tinfos):
     if attrs.get_bool('use_bias'):
         bias = inputs[2]
         bias = sym.reshape(bias, shape=(OC, oc_bn))
-        return sym.conv2d_nChwc(data_sym, trans_kernel, bias, **new_attrs)
+        return sym.contrib.conv2d_nchw_kernel_packed(inputs[0], trans_kernel, bias, **new_attrs)
     else:
-        return sym.conv2d_nChwc(data_sym, trans_kernel, **new_attrs)
+        return sym.contrib.conv2d_nchw_kernel_packed(inputs[0], trans_kernel, **new_attrs)
 
 
-@reg.register_weight_prepack("max_pool2d")
-def max_pool2d_callback(attrs, inputs, tinfos):
-    new_attrs = {k: attrs[k] for k in attrs.keys()}
-    new_attrs['layout'] = 'NCHW16c'
-    return sym.max_pool2d(inputs[0], **new_attrs)
-
-
-@reg.register_weight_prepack("avg_pool2d")
-def avg_pool2d_callback(attrs, inputs, tinfos):
-    new_attrs = {k: attrs[k] for k in attrs.keys()}
-    new_attrs['layout'] = 'NCHW16c'
-    return sym.avg_pool2d(inputs[0], **new_attrs)
-
-
-@conv2d_nChwc.register("cpu", override=True)
+@conv2d_nchw_kernel_packed.register("cpu", override=True)
 def _declaration_conv(data, kernel, num_filter, kernel_size, stride, padding, out_dtype):
     assert data.shape[0].value == 1, "only support batch size=1 convolution on avx"
-    n, ic_chunk, h, w, ic_block = [x.value for x in data.shape]
-    ic = ic_chunk * ic_block
+    _, ic, _, _ = [x.value for x in data.shape]
     oc = num_filter
     kh, kw = kernel_size
-    wkl = _get_workload(tvm.placeholder((n, ic, h, w), dtype=out_dtype),
-                        tvm.placeholder((oc, ic, kh, kw), dtype=out_dtype), stride, padding, out_dtype)
+    wkl = _get_workload(data, tvm.placeholder((oc, ic, kh, kw)), stride, padding, out_dtype)
     sch = _get_schedule(wkl)
     return _SCH_TO_DECL_FUNC[type(sch)](data, kernel, stride, padding, out_dtype)
 
 
-@generic.schedule_conv2d_nChwc.register(["cpu"], override=True)
-def schedule_conv2d_nChwc(num_filter, kernel_size, outs):
+@generic.schedule_conv2d_nchw_kernel_packed.register(["cpu"], override=True)
+def schedule_conv2d(num_filter, kernel_size, outs):
     """Create schedule for tensors"""
     s = tvm.create_schedule([x.op for x in outs])
 
@@ -604,49 +611,33 @@ def schedule_conv2d_nChwc(num_filter, kernel_size, outs):
                 if tensor.op.input_tensors:
                     traverse(tensor.op)
 
-        if 'conv2d_nChwc' in op.tag:
+        if 'conv2d_nchw' in op.tag:
             output = op.output(0)
-            # conv_out = op.input_tensors[0]
-            conv_out = output
+            conv_out = op.input_tensors[0]
             kernel = conv_out.op.input_tensors[1]
             # kernel = kernel_vec.op.input_tensors[0]
             data_vec = conv_out.op.input_tensors[0]
-            data = data_vec.op.input_tensors[0] \
-                if isinstance(data_vec.op, tvm.tensor.ComputeOp) and len(data_vec.op.input_tensors) > 0 and "pad" not in data_vec.op.tag \
-                else data_vec
+            data = data_vec.op.input_tensors[0]
             data_pad = None
             if isinstance(data.op, tvm.tensor.ComputeOp) and "pad" in data.op.tag:
                 data_pad = data
                 data = data_pad.op.input_tensors[0]
+            padding = infer_pad(data, data_pad)
 
-            n, ic_chunk, h, w, ic_block = [x.value for x in data.shape]
-            ic = ic_chunk * ic_block
-            original_data = tvm.placeholder((n, ic, h, w), dtype=output.dtype)
-
-            if data_pad is not None:
-                n, _, pad_h, pad_w, _ = [x.value for x in data_pad.shape]
-                original_data_pad = tvm.placeholder((n, ic, pad_h, pad_w), dtype=output.dtype)
-                padding = infer_pad(original_data, original_data_pad)
-            else:
-                padding = (0, 0)
-
+            _, ic, _, _ = [x.value for x in data.shape]
             oc = num_filter
             kh, kw = kernel_size
-            original_kernel = tvm.placeholder((oc, ic, kh, kw), dtype=output.dtype)
-
-            n, oc_chunk, oh, ow, oc_block = [x.value for x in output.shape]
-            original_output = tvm.placeholder((n, oc_chunk*oc_block, oh, ow), dtype=output.dtype)
+            original_kernel = tvm.placeholder((oc, ic, kh, kw))
 
             if data_pad is None:
-                stride = infer_stride(original_data, original_kernel, original_output)
+                stride = infer_stride(data, original_kernel, output)
             else:
-                stride = infer_stride(original_data_pad, original_kernel, original_output)
+                stride = infer_stride(data_pad, original_kernel, output)
 
-            wkl = _get_workload(original_data, original_kernel, stride, padding, output.dtype)
+            wkl = _get_workload(data, original_kernel, stride, padding, output.dtype)
             sch = _get_schedule(wkl)
             _SCH_TO_SCH_FUNC[type(sch)](s, data, data_pad, data_vec,
                                         kernel, conv_out, output, outs[0])
-
 
     traverse(outs[0].op)
     return s
